23a77b681aba2e58b2d7464a71a63bf639ded6f2
Hygor Luciano <hygorluciano0@gmail.com>
2023-07-17 10:15:51 -0300

    create migration V2

--- src/main/java/com/example/crud/domain/product/Product.java
+++ src/main/java/com/example/crud/domain/product/Product.java
@@ -1,11 +1,28 @@
 package com.example.crud.domain.product;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
-import lombok.EqualsAndHashCode;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Table(name = "product")
 @Entity(name = "product")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @EqualsAndHashCode(of = "id") //fala pro spring a PK o primekey das tabela
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    private String name;
+
+    private Integer price_in_cents;
+
+    public Product(RequestProductDto requestProductDto){
+        this.name = requestProductDto.name();
+        this.price_in_cents = requestProductDto.price_in_cents();
+    }
 }
+
